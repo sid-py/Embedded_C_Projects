56701e2a6c16ef9b58d67455fe375581bfe23c2c
Sid <64964929+sid-py@users.noreply.github.com>
2021-11-15 22:20:41 +0100

    emb

--- My_workspace/target/012LED_toggle2_struct/Src/main.c
+++ My_workspace/target/012LED_toggle2_struct/Src/main.c
@@ -9,15 +9,9 @@
 # define AHB1PERIPH_BASE 		(PERIPH_BASE + AHB1PERIPH_OFFSET)
 # define RCC_BASE		 		(0x40023800UL)
 # define GPIOD_BASE 			(0x40020C00UL)
-# define AHB1EN_R_OFFSET 		(0x30UL)
-# define RCC_AHB1EN_R 			(*(volatile unsigned int *)(RCC_BASE + AHB1EN_R_OFFSET))
-# define MODE_R_OFFSET 			(0x00UL)
-#define GPIOD_MODE_R 			(*(volatile unsigned int *)(GPIOD_BASE + MODE_R_OFFSET))
-
-#define OD_R_OFFSET 			(0x14UL)
-# define GPIOD_OD_R 			(*(volatile unsigned int *)(GPIOD_BASE + OD_R_OFFSET))
 # define GPIODEN 				(1U<<3)
 
+# define __IO volatile // Misra rule to define volatile as __IO
 
 #define LED_PIN 				(1U<<12)
 
@@ -25,26 +19,36 @@
 //(1U << 24) // set bit 24 to 1
 //&=~(1U<<25) // set bit 25 to 0
 
+typedef struct
+{
+	__IO uint32_t MODER; // GPIO port mode register
+	__IO uint32_t DUMMY[4]; // GPIO O/P type register
+
+	__IO uint32_t ODR; // GPIO output data register
+
+
+} GPIO_TypeDef;
+
+typedef struct
+{
+	__IO uint32_t DUMMY [12]; // Dummy register to hold 12 register places
+	__IO uint32_t AHB1ENR // RCC AHB1 peripheral clock register
+} RCC_TypeDef;
+
+# define RCC 		((RCC_TypeDef*)RCC_BASE)
+# define GPIOD 		((GPIO_TypeDef*)GPIOD_BASE)
 
 int main(void)
 {
 	//1. Enable clock access to GPIOD
-	RCC_AHB1EN_R |= GPIODEN;
-
+	RCC->AHB1ENR |= GPIODEN;
 	//2. Set LED_PIN as output pin
-	GPIOD_MODE_R |= (1U << 24); // set bit 24 to 1
-	GPIOD_MODE_R &=~(1U<<25); // set bit 25 to 0
+	GPIOD->MODER |= (1U << 24); // set bit 24 to 1
+	GPIOD->MODER &=~(1U<<25); // set bit 25 to 0
 
 	while(1)
 	{
-		//3. Set LED_PIN high
-		//GPIOD_OD_R |= LED_PIN;
-
-		//4. Toggle LED Pin
-		GPIOD_OD_R ^= LED_PIN;
-
+		GPIOD->ODR ^= LED_PIN;
 		for (int i =0; i<100000; i++);
-
-
 	}
 }
